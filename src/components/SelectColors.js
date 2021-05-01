0791839079bd6f493b532264b91ec0cceb843732
seilylook@naver.com <seilylook@naver.com>
2021-05-01 22:28:52 +0900

    select-colors

---
+++ src/components/SelectColors.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+const SelectColors = () => {
+  return (
+    <div>
+      <h2>색상을 선택하세요</h2>
+    </div>
+  );
+};
+
+export default SelectColors;
